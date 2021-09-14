b2200f60f2241655ff9b6b6a275474c285c37cb3
BronsonRice <89786566+BronsonRice@users.noreply.github.com>
2021-09-14 23:11:31 +0000

    updated lines 1-19

--- main.cs
+++ main.cs
@@ -2,31 +2,27 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    /* Declaring variables */
-    int num1 = 15;
-    int num2 = 8;
-    int sum= 23;
-    int diff = 7;
-    int product = 120;
-    int quotient = 1;
-    int remainder = 7;
+    Console.WriteLine ("Enter a number");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter another number");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int sum = num1 + num2;
+    int diff = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1/ num2;
+    int remainder = num1 % num2;
 
-
-    /* Output statements */
-    Console.WriteLine("Enter a number:" + num1);
-    Console.WriteLine("Enter another number:" + num2);
     Console.WriteLine("sum=" + sum);
-    Console.WriteLine("diff=" + diff);
-    Console.WriteLine("product=" + product);
-    Console.WriteLine("quotient=" + quotient);
-    Console.WriteLine("remainder=" + remainder);
+    Console.WriteLine("diff=" + diff );
+    Console.WriteLine("product="+ product);
+    Console.WriteLine("quotient="+ quotient);
+    Console.WriteLine("remainder="+ remainder);
 
-    /* Arithmetic operators */
-    sum = 15 + 8;
-    diff = 15 - 8;
-    product = 23 * 7;
-    quotient = 15/8;
-    remainder = 15 % 8;
 
+    
+
+
+    
+    
   }
 }
